519c1f3db51cbe7852d18dd28276e6a5424911e8
Juliane Marques <mqs.juliane@gmail.com>
2021-08-09 17:10:08 -0300

    Cria mensagem e atributos para requisito 2

---
+++ src/pages/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+              <p data-testid="home-initial-message">
+                Digite algum termo de pesquisa ou escolha uma categoria.
+              </p>
+            </div>
+        )
+    }
+}
+
+
+
+export default Main;

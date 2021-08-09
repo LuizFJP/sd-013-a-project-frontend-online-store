46080c319f6368814ef103be0dc79a65745dbfd7
Gustavo Pia <piagja@gmail.com>
2021-08-09 18:02:33 -0300

    Realizou o requisito 2 - Renderizou no App um input de busca

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import InputSearch from './components/InputSearch/InputSearch';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Frontend Online Store</h1>
+        <InputSearch />
       </div>
     );
   }

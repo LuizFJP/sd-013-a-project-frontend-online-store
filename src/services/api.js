07908f6852ded1a0a9793d54c31b6741d0485efb
Gustavo Pia <piagja@gmail.com>
2021-08-09 17:20:57 -0300

    Realizou o requisito 01 - módulo da API do ML

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const fetchCategories = await fetch(URL);
+  const response = await fetchCategories.json();
+
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const fetchApi = await fetch(URL);
+  const response = await fetchApi.json();
+
+  return response;
 }

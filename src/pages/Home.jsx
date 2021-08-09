36be41aa2258e4318f9767d8817c02e2513fb365
Filipe <filipecandido123@hotmail.com>
2021-08-09 17:20:32 -0300

    requisito 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import imgCarShop from '../image/shopping-cart_icon-icons.com_60593.svg';
 // Rodrigo Pova, Luiz Furtado, Filipe Cândido, Felipe Chagas, Danilo Uehara
 class Home extends React.Component {
   render() {
     return (
-      <label htmlFor="input-search" data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-        <input id="input-search" type="text" />
-      </label>
+      <div>
+        <label htmlFor="input-search" data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+          <input id="input-search" type="text" />
+        </label>
+        <Link data-testid="shopping-cart-button" to="/carshop">
+          <img src={ imgCarShop } alt="carrinho de compras" />
+        </Link>
+      </div>
     );
   }
 }

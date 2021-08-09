d7e52e6978194a242dc96a36f388e47d5f9c00d2
Rodrigo Pova <rodrigopova94@gmail.com>
2021-08-09 17:36:27 -0300

    Requisito 4

---
+++ src/pages/Categories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { getCategories } from '../services/api';
+// Rodrigo Pova
+
+class Categories extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.updateState();
+  }
+
+  updateState = async () => {
+    this.setState({
+      categories: await getCategories(),
+    });
+  }
+
+  render() {
+    const { categories } = this.state;
+    console.log(categories);
+    return (
+      <ul>
+        {categories.map((category) => (
+          <li key={ category.id } data-testid="category">
+            {category.name}
+          </li>))}
+      </ul>
+    );
+  }
+}
+
+export default Categories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Categories from './Categories';
 // Rodrigo Pova, Luiz Furtado, Filipe Cândido, Felipe Chagas, Danilo Uehara
 class Home extends React.Component {
   render() {
     return (
-      <label htmlFor="input-search" data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-        <input id="input-search" type="text" />
-      </label>
+      <div>
+        <Categories />
+        <label htmlFor="input-search" data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+          <input id="input-search" type="text" />
+        </label>
+      </div>
     );
   }
 }

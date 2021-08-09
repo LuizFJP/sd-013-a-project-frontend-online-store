a7703dc7ec099d47371d7c7396b5fda77d019815
Luiza Antiques <luiza.antiques@gmail.com>
2021-08-09 18:24:52 -0300

    requirement 03

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import SearchInput from './components/SearchInput';
+import { SearchInput, ShoppingCart } from './components';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ SearchInput } />
+        <Route path="/mycart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );

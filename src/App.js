2f728d8f24fbecbc441aaf13072694aa3342a286
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-08-09 18:07:06 -0300

    teste requisito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <p> OI </p>
-        {/* apagar */}
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" component={ Home } />
+        </Switch>
+      </Router>
     );
   }
 }

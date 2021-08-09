064a7ed484ca96908dde62aba3156756eef49693
sonkai <mfilho86@gmail.com>
2021-08-09 17:25:36 -0300

    req 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CategoriesList from '../components/CategoriesList';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <>
+      <main className="home-container">
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
         <Link to="/cart">
           <button data-testid="shopping-cart-button" type="button">Shopping Cart</button>
         </Link>
-      </>
+        <aside>
+          <CategoriesList />
+        </aside>
+      </main>
     );
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,11 @@
 export async function getCategories() {
   const data = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const dataJson = await data.json();
-
   return dataJson;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const responseJson = await response.json();
-
   return responseJson;
 }

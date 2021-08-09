3c0764fe78095a1642d8704fb0e1a18973e49591
Heleny Flavia <flaviaanahmias@gmail.com>
2021-08-09 17:25:23 -0400

    Requisito 3 - corrige lint

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 
-
 class Cart extends Component {
-    render(){
-        return(
-            <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-        );
-    }
+  render() {
+    return (
+      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    );
+  }
 }
 
 export default Cart;

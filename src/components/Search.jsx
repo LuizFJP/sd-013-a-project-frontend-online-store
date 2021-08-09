2297061416e96d177d5ec712c239ada5bb3bb6dd
Gessé Carlos <gesse.dev@outlook.com>
2021-08-09 16:02:53 -0300

    Requisito 2 feito

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Search extends React.Component {
+  render() {
+    return (
+      <h2 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h2>
+    );
+  }
+}

e99e545b784895388cc05a91248e4df3deb20551
ALINE <hoshinoaline@yahoo.com.br>
2021-08-09 15:42:07 -0300

    cria componente de busca

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 export async function getCategories() {
-  // Implemente aqui
+  const api = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const Obj = await api.json();
+  return Obj;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {

62f6e841e31fc2e3ee776ec91b0bd9ecc78d1b92
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-08-09 18:42:26 -0300

    cria icone de carrinho e pagina carrinho

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route path="/Cart" component={ Cart } />
       </Switch>
     </BrowserRouter>
   );

0c731ab4b51fe5851a77946508e09805ffa39059
Andrey Gois <gois.andrey@hotmail.com>
2021-08-09 16:32:52 -0300

    Adiciona questao 02

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+
+    );
+  }
+}
+
+export default SearchBar;

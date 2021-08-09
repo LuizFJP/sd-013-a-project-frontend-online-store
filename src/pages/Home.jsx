6868b2e03f85bdfb94e80e42d44906d2e7a114d6
JeanVPacheco <jeanvitorp@gmail.com>
2021-08-09 19:26:01 -0300

    Adiciona CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import Categories from '../components/Categories';
 import '../style.css/home.css';
 import { FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import Categories from '../components/Categories';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <header><h1>Mockado Livre</h1></header>
-        <input className="searchBar" type="text" />
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <div className="searchArea">
+          <input className="searchBar" type="text" />
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </div>
         <Categories />
         <Link data-testid="shopping-cart-button" to="/Cart">
           <FaShoppingCart size={ 20 } color="grey" />

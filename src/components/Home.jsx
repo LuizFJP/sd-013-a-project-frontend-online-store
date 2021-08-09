8f71981775a2146a201fe89a7b8d18bd1bc49707
João Victor Paduam <joao.paduam@hotmail.com>
2021-08-09 20:04:34 -0300

    Finalizando requisito 4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
+import CategoriesList from './CategoriesList';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+    this.categories = this.categories.bind(this);
+  }
+
+  componentDidMount() {
+    this.categories();
+  }
+
+  async categories() {
+    const categories = await getCategories();
+    console.log(categories);
+    this.setState({
+      categories,
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
+        <CategoriesList categories={ categories } />
         <form>
           <label
             type="text"

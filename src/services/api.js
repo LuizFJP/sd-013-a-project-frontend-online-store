df12f1b80bfd83157e1f4fd04b5bc281c70c31c0
Rafael Janovicci <rjanovicci@hotmail.com>
2021-08-09 17:07:30 -0300

    implementa funçoes da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+    return (await fetch(url)).json();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+    return (await fetch(url)).json();
+  } catch (error) {
+    console.log(error);
+  }
 }

efd6442b32096be0a793170573706db095aad182
JeanVPacheco <jeanvitorp@gmail.com>
2021-08-09 16:14:19 -0300

    Adiciona funcionalidade de barra de procura

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../style.css/home.css'
+export default class Home extends Component {
+    render(){
+        return (
+            <div>
+                <header><h1>Mockado Livre</h1></header>
+                <input className= "searchBar"type='text' />
+                <p data-testid='home-initial-message'>"Digite algum termo de pesquisa ou escolha uma categoria."</p>
+            </div>
+        )
+    }
+}

2a6783d6343dadc492843310b713107fb68a5a9b
Matheus Duarte de Freitas <freickup@gmail.com>
2021-08-10 13:24:52 -0300

    criacao do estado da home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,17 @@ import { Link } from 'react-router-dom';
 import Category from '../components/Category';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      categoria: '',
+      noSearch: true,
+      products: [],
+      inCart: 0,
+    };
+  }
+
   render() {
     return (
       <div>

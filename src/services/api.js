8a0cdb5da2edf705033a9053f4256226e4f8393e
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-08-10 17:01:23 -0300

    Completa o primeiro e o segundo requisito, com alguns erros no primeiro requisito ainda.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,23 @@
 export async function getCategories() {
-  // Implemente aqui
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((r) => r.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (categoryId && !query) {
+    return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`)
+      .then((r) => r.json())
+      .then((r) => r.results);
+  }
+  if (!categoryId && query) {
+    return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+      .then((r) => r.json())
+      .then((r) => r.results);
+  }
+  if (!categoryId && !query) {
+    return {};
+  }
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+    .then((r) => r.json())
+    .then((r) => r.results);
 }

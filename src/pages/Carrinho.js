ceb4d8732fcbc642c73b45018756eb441cb2e861
Diego Vilela <vilelaphp@gmail.com>
2021-08-10 15:27:20 -0300

    [Grupo 2] Implementa o requisito 3

---
+++ src/pages/Carrinho.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Carrinho extends React.Component {
+  render() {
+    return <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+  }
+}
+
+export default Carrinho;

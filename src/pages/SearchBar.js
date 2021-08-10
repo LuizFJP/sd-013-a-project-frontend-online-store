e4528b9a335f14701dc72b0f48e21597bb2a7dc4
Isabella Antunes <bella.antunes2@gmail.com>
2021-08-10 15:29:42 -0300

    Requisito 4

--- src/pages/SearchBar.js
+++ src/pages/SearchBar.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import Button from '../Components/Button';
+import * as api from '../services/api';
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories().then((category) => this.setState({
+      categories: category,
+    }));
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <label data-testid="home-initial-message" htmlFor="initial-message">
@@ -13,6 +29,14 @@ class SearchBar extends React.Component {
         <div>
           <Button />
         </div>
+        <nav>
+          <ul>
+            { categories.map((category) => (
+              <li key={ category.id } data-testid="category">
+                {category.name}
+              </li>))}
+          </ul>
+        </nav>
       </div>
     );
   }

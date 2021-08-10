3c7b97419de04a30e38aa020e3941f26f43ace51
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-08-10 18:04:18 -0300

    Completa o terceiro requisito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Body from './components/Body';
+import ShoppingCart from './components/ShoppingCart';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Body />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/shopping-cart" component={ ShoppingCart } />
+            <Route exact path="/" component={ Body } />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }

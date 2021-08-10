ada2ba36e8fbd4da7078ed8ceb2316a074a3e84f
Juliane Marques <mqs.juliane@gmail.com>
2021-08-10 14:37:40 -0300

    comenta link api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Category from '../components/Category';
+// import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Main extends Component {
   constructor(props) {
@@ -12,12 +13,46 @@ class Main extends Component {
       products: [],
       inCart: 0,
     };
+  };
+
+  handleChangeInput = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      inputValue: value,
+    });
+  }
+
+  requestProducts = async () => {
+    const { inputValue, categoria } = this.state;
+    if (inputValue) {
+      this.setState({
+        products: []
+      })
+      const request = await api.getProductsFromCategoryAndQuery(categoria, inputValue)
+      const produtos = request.results;
+        this.setState({
+          products: [...produtos],
+          noSearch: false,
+        });
+    };
   }
 
   render() {
     return (
       <div>
-        <input />
+        <input 
+        data-testid="query-input" 
+        type="text"
+        name="product-search"
+        onChange={ this.handleChangeInput }
+        value={ inputValue }
+         />
+        <button 
+        data-testid="query-button" 
+        onClick={ this.requestProducts } 
+        type="button">
+          Buscar
+        </button>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>

ed529eb19f97a9c2e7442d3a2971b7672580f014
Filipe Cândido <70904498+Fedolfo@users.noreply.github.com>
2021-08-10 14:42:47 -0300

    Update ListByCategory.jsx
    Retirado as propTypes de listByCategory

--- src/pages/ListByCategory.jsx
+++ src/pages/ListByCategory.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import Home from './Home';
 
 class ListByCategory extends React.Component {
@@ -9,13 +8,5 @@ class ListByCategory extends React.Component {
     );
   }
 }
-
-// ListByCategory.propTypes = {
-//   match: PropTypes.shape({
-//     params: PropTypes.shape({
-//       category: PropTypes.string,
-//     }),
-//   }).isRequired,
-// };
 
 export default ListByCategory;

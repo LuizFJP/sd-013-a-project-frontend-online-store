05f18a83786fd26b6668bb011e3f8bf129939903
luiz furtado <luizfurtadomkt@hotmail.com>
2021-08-10 11:03:56 -0300

    ultimas alteracoes de ontem

--- src/pages/ListByCategory.jsx
+++ src/pages/ListByCategory.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Home from './Home';
-import * as API from '../services/api';
 
 class ListByCategory extends React.Component {
-  test = async () => {
-    await API.getProductsFromCategoryAndQuery('categoryId', 'query');
-  }
-
-  componentDidMount() {
-    this.test();
-  }
 
   render() {
     return (
@@ -19,12 +11,12 @@ class ListByCategory extends React.Component {
   }
 }
 
-ListByCategory.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      category: PropTypes.string,
-    }),
-  }).isRequired,
-};
+// ListByCategory.propTypes = {
+//   match: PropTypes.shape({
+//     params: PropTypes.shape({
+//       category: PropTypes.string,
+//     }),
+//   }).isRequired,
+// };
 
 export default ListByCategory;

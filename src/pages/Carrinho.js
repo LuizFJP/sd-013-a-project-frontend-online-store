0fc3b9578746cc79949a0faa963ec457c0dbab7e
Diego Vilela <vilelaphp@gmail.com>
2021-08-10 15:48:18 -0300

    [Grupo 2] Corrige lint

--- src/pages/Carrinho.js
+++ src/pages/Carrinho.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 class Carrinho extends React.Component {
   render() {
-    return <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+    return (
+      <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+    );
   }
 }
 

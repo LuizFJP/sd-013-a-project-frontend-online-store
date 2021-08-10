735acf3dfcbed42071934d9797582f044a04e05d
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-08-10 17:38:17 -0300

    Corrige lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { getProductsFromCategoryAndQuery } from './services/api';
 import Body from './components/Body';
 import './App.css';
 
 class App extends React.Component {
-  componentDidMount() {
-    getProductsFromCategoryAndQuery('MLB1132', 'molho de tomate');
-  }
-
   render() {
     return (
       <div className="App">

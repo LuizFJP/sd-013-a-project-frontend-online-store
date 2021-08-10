f43cbc903421d0d6ec6221bc60747df3b0ad0be7
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2021-08-10 18:21:31 -0300

    requisito 6 completo

--- src/pages/Categorias.js
+++ src/pages/Categorias.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { getCategories } from '../services/api';
 
 class Categorias extends Component {
@@ -16,20 +17,36 @@ class Categorias extends Component {
 
   setCategorias = async () => {
     const categoriasJson = await getCategories();
+    console.log(categoriasJson);
     this.setState({ categorias: categoriasJson });
   }
 
   render() {
     const { categorias } = this.state;
+    const { atualizaCategoriaId } = this.props;
     return (
       <div>
         <ul>
           {categorias
-            .map(({ name, id }) => <li data-testid="category" key={ id }>{ name }</li>)}
+            .map(({ name, id }) => (
+              <button
+                type="button"
+                onClick={ atualizaCategoriaId }
+                data-testid="category"
+                key={ id }
+                id={ id }
+              >
+                { name }
+              </button>
+            ))}
         </ul>
       </div>
     );
   }
 }
+
+Categorias.propTypes = {
+  atualizaCategoriaId: PropTypes.func.isRequired,
+};
 
 export default Categorias;

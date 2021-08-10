4e1fd17454d812abeb38c5429a2012f613a13aa5
turtore <brunosap@gmail.com>
2021-08-10 15:38:53 -0300

    atendido requisito3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import SearchBar from './Components/SearchBar';
+import Home from './Components/Home';
+import ShoppingCart from './Components/ShoppingCart';
+// import SearchBar from './Components/SearchBar';
 // import * as api from './services/api';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Route exact path="/" component={ SearchBar } />
+        <Route exact path="/" component={ Home } />
+        <Route path="/cart" component={ ShoppingCart } />
       </BrowserRouter>
     </div>
   );

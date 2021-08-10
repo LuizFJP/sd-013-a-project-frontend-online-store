c460d6490abc2ffb52dde7521f34cf7a61a20e67
JeanVPacheco <jeanvitorp@gmail.com>
2021-08-10 14:35:08 -0300

    Ajeita lint

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,14 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <header><h1>Mockado Livre</h1></header>
+        <header>
+          <h1>Mockado Livre</h1>
+          <div className="leftSide">
+            <Link data-testid="shopping-cart-button" to="/Cart">
+              <FaShoppingCart className="cart" />
+            </Link>
+          </div>
+        </header>
         <div className="searchArea">
           <input className="searchBar" type="text" />
           <p data-testid="home-initial-message">
@@ -16,9 +23,16 @@ export default class Home extends Component {
           </p>
         </div>
         <Categories />
-        <Link data-testid="shopping-cart-button" to="/Cart">
-          <FaShoppingCart size={ 20 } color="grey" />
-        </Link>
+        <footer>
+          <h5>
+            Desenvolvido por:
+            <a href="https://github.com/cae07" target="_blank" rel="noopener noreferrer"> Caê</a>
+            <a href="https://github.com/ogustavomauricio" target="_blank" rel="noopener noreferrer"> Gustavo </a>
+            <a href="https://github.com/JeanVPacheco" target="_blank" rel="noopener noreferrer"> Jean </a>
+            <a href="https://github.com/pabloalmeidac" target="_blank" rel="noopener noreferrer"> Pablo</a>
+            <p>Projeto para formação no curso de Desenvolvedor Web da Trybe</p>
+          </h5>
+        </footer>
       </div>
     );
   }

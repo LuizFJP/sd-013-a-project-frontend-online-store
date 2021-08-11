e374c1dd1cf94d8b135de17ee0973006ffcac8bb
Gustavo-Jezini <gustavojezini@gmail.com>
2021-08-11 15:07:45 -0300

    Tentando mergear

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Link,
-  Route,
-} from 'react-router-dom';
-
-import Home from './pages/Home';
-import ShoppingCart from './pages/ShoppingCart';
-
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/shopping" data-testid="shopping-cart-button">Cart</Link></li>
-        </ul>
-=======
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -37,22 +16,5 @@ function App() {
     </div>
   );
 }
->>>>>>> main-group-15-reset
 
-        <Switch>
-          <Route
-            exact
-            path="/shopping"
-            component={ ShoppingCart }
-          />
-
-          <Route
-            exact
-            path="/"
-            component={ Home }
-          />
-        </Switch>
-      </Router>
-    );
-  }
-}
+export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,4 @@
 export async function getCategories() {
-<<<<<<< HEAD
-  try {
-    const fetchAPI = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-    const categories = await fetchAPI.json();
-    return categories;
-  } catch (error) {
-    return error;
-  }
-}
-
-export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  try {
-    const fetchAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
-    const categoryAndQuery = await fetchAPI.json();
-    return categoryAndQuery;
-  } catch (error) {
-    return error;
-=======
   // Implemente aqui
   try {
     const fetchAPI = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
@@ -27,7 +9,7 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   }
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query ) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   try {
     const fetchAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
@@ -35,6 +17,5 @@ export async function getProductsFromCategoryAndQuery(categoryId, query ) {
     return json;
   } catch (error) {
     console.log(error);
->>>>>>> main-group-15-reset
   }
 }

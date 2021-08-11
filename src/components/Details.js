732588d50fbf4f116074b3d3029e9046b6e2e392
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-08-11 13:11:40 -0300

    Requisito 07

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      product: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchProduct();
+  }
+
+  fetchProduct = async () => {
+    const { match: { params: { id } } } = this.props;
+    const url = `https://api.mercadolibre.com/items/${id}`;
+    const response = await fetch(url);
+    const details = await response.json();
+    this.setState({
+      product: details,
+      loading: false,
+    });
+  }
+
   render() {
+    const { product, loading } = this.state;
+    if (loading) return <div>Carregando...</div>;
+
     return (
       <div>
-        <p>Trybe</p>
+        <span data-testid="product-detail-name">{ product.title }</span>
+        <img src={ product.thumbnail } alt="product details" />
+        <span>{ product.price }</span>
+        <h4>Especificações Técnicas</h4>
+        <Link
+          to="/shoppingcart"
+        >
+          <button type="button">
+            Adicione no Carrinho
+            <span>🛒</span>
+          </button>
+        </Link>
+        <Link
+          to="/"
+        >
+          <button type="button">
+            Voltar
+          </button>
+        </Link>
       </div>
     );
   }
 }
+
+Details.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Details;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductCard extends Component {
   render() {
-    const { item: { title, thumbnail, price } } = this.props;
+    const { item: { title, thumbnail, price, id } } = this.props;
     return (
-      <div data-testid="product">
-        <span>{ title }</span>
-        <img src={ thumbnail } alt={ title } />
-        <span>{ price }</span>
-      </div>
+      <Link
+        to={ `/details/${id}` }
+        data-testid="product-detail-link"
+      >
+        <div data-testid="product">
+          <span>{ title }</span>
+          <img src={ thumbnail } alt={ title } />
+          <span>{ price }</span>
+        </div>
+      </Link>
     );
   }
 }
@@ -19,6 +25,7 @@ ProductCard.propTypes = {
     title: PropTypes.string,
     thumbnail: PropTypes.string,
     price: PropTypes.number,
+    id: PropTypes.string,
   }).isRequired,
 };
 

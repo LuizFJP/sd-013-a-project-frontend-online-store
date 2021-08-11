06ce19fab33eba7932f673fd1307401098d9d913
EmmanuelBMS <acquabriall2020@gmail.com>
2021-08-11 01:28:18 -0300

    adiciona imagem de carrinho no botao

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import cartimg from '../cartimg.png';
 
 class SearchBar extends React.Component {
   render() {
@@ -15,7 +16,16 @@ class SearchBar extends React.Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </span>
         <div>
-          <Link data-testid="shopping-cart-button" to="/shopping-cart">cart</Link>
+          <Link
+            data-testid="shopping-cart-button"
+            to="/shopping-cart"
+          >
+            <img
+              src={ cartimg }
+              alt="carrinho de compras"
+              className="shopping-cart-button"
+            />
+          </Link>
         </div>
       </div>
     );

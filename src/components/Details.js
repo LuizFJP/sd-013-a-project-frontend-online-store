fedc086ccf6f2c552631c9cf69e307c99c2d5c37
FelipeNevess <felipe012neves@gmail.com>
2021-08-11 17:59:12 -0300

    Corrigindo erro de lint

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Review } from './index';
+import Review from './Review';
 
 class Details extends Component {
   constructor(props) {
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Review } from './Review';
 export { default as Details } from './Details';
 export { default as ShoppingCart } from './ShoppingCart';
 export { default as Home } from './Home';
